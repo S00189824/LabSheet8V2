3d957588187a5fd47fed402caf5183c4b3c533c3
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-30 01:55:36 +0100

    zfdsd

---
+++ Labsheet8V2/Exercise2/ComputerGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    public class ComputerGame
+    {
+        public int GameID { get; set; }
+        public string GameName { get; set; }
+        public int YearReleased { get; set; }
+        public string Genre { get; set; }
+
+        
+        public virtual List<Character> Characters { get; set; }
+    }
+
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        public int GameID { get; set; }
+        public string CharacterName { get; set; }
+        public string CharacterImage  { get; set; }
+
+        public virtual ComputerGame ComputerGame { get; set; }
+    }
+
+    public class ComputerGameData : DbContext
+    {
+        public ComputerGameData() : base("MyComputerGames") { }
+
+        public DbSet<ComputerGame> Games { get; set; }
+        public DbSet<Character> Characters { get; set; }
+    }
+}
---
+++ Labsheet8V2/GameManagement/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exercise2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerGameData db = new ComputerGameData();
+
+            using (db)
+            {
+                ComputerGame g1 = new ComputerGame { GameID = 1, GameName = "Borderlands", YearReleased = 2019, Genre = "FPS" };
+                Character p1 = new Character { CharacterID = 1, CharacterName = "Flak", CharacterImage = "c1", ComputerGame = g1,GameID = 1 };
+                Character p2 = new Character { CharacterID = 2, CharacterName = "Amara", CharacterImage = "c4", ComputerGame = g1,GameID = 1 };
+
+                ComputerGame g2 = new ComputerGame { GameID = 2, GameName = "Gran Blue Fantasy", YearReleased = 2020, Genre = "RPG" };
+                Character p3 = new Character { CharacterID = 3, CharacterName = "Emilia", CharacterImage = "c3",ComputerGame = g2,GameID = 2 };
+                Character p4 = new Character { CharacterID = 4, CharacterName = "Tom", CharacterImage = "c2",ComputerGame = g2,GameID = 2 };
+
+                db.Games.Add(g1);
+                db.Games.Add(g2);
+
+                Console.WriteLine("Added Game to Database");
+
+                db.Characters.Add(p1);
+                db.Characters.Add(p2);
+                db.Characters.Add(p3);
+                db.Characters.Add(p4);
+
+                Console.WriteLine("Added Characters to database");
+
+                db.SaveChanges();
+
+                Console.WriteLine("Saved Changed to database");
+            }
+        }
+    }
+}
